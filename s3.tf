c167e22e43fc19a8152e52117ed0aedf6e927341
Amrit Rathore <amritrathore@Mac-2.broadband>
2025-04-09 12:34:46 +0100

    changes

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "amritb1" {
-  bucket = "amrit-test-bucket-1"
+  bucket = "amrit-test-bucket-3"
   }
 
 resource "aws_s3_bucket" "amritb2" {
-  bucket = "amrit-test-bucket-2"
+  bucket = "amrit-test-bucket-4"
   }
   
