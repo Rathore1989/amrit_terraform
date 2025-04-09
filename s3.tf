0b4946340a811dfc8c602fed2f7efacbb3609f2b
Amrit Rathore <amritrathore@Mac-2.broadband>
2025-04-09 14:55:31 +0100

    new dev s3 changes

--- s3.tf
+++ s3.tf
@@ -1,12 +1,3 @@
 resource "aws_s3_bucket" "amritb111" {
   bucket = "amrit-test-bucket-111"
   }
-
-resource "aws_s3_bucket" "amritb222" {
-  bucket = "amrit-test-bucket-222"
-  }
-
-resource "aws_s3_bucket" "amritb333" {
-  bucket = "amrit-test-bucket-333"
-  }
-  
