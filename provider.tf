74c7ba20b9c09ae16006ab72e91573b488f7f073
Amrit Rathore <amritrathore@Mac-2.broadband>
2025-04-09 12:10:44 +0100

    initial command

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.94.1"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.akey
+  secret_key = var.skey
+  region = var.region
+}
+
+terraform { 
+  cloud { 
+    organization = "amrit-terraform" 
+    workspaces { 
+      name = "project1-ws1" 
+    } 
+  } 
+}
---
+++ s3.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "amritb1" {
+  bucket = "amrit-test-bucket-1"
+  }
+
+resource "aws_s3_bucket" "amritb2" {
+  bucket = "amrit-test-bucket-2"
+  }
+  
